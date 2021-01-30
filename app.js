aba0a04f6d469e61b1f6b62023a311e316eee902
mini4614 <mini4614@gmail.com>
2021-01-31 02:16:24 +0900

    disign

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const userRoutes = require('./routes/users');
 
 //mongo
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/remotto'; //process.env.DB_URL;
+const dbUrl ='mongodb://localhost:27017/remotto' || process.env.DB_URL;
 //console.log(dbUrl)
 //mongodb://localhost:27017/yelp-camp
 mongoose.connect(dbUrl, {
